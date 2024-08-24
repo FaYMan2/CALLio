c0b36494ce7372576b4c78d7b2ff9b498b25e4be
FaYMan2 <suavarnavats244@gmail.com>
2024-08-24 21:18:43 +0530

    unused import

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-  // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore,collection,addDoc,setDoc,doc,onSnapshot,getDoc, enablePersistentCacheIndexAutoCreation, updateDoc} from "firebase/firestore";
+import { getFirestore,collection,addDoc,setDoc,doc,onSnapshot,getDoc, updateDoc} from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app)
 
